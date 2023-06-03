e5fdda65ed0a1726478dc13375756005730cf3d3
Nickolas-Belo <79777976+Nickolas-Belo@users.noreply.github.com>
2023-06-03 20:43:23 -0300

    Add files via upload
    Commit do Teste Unitário da classe ProfessorEntidade

--- src/test/org/fpij/jitakyoei/model/beans/ProfessorEntidadeTest.java
+++ src/test/org/fpij/jitakyoei/model/beans/ProfessorEntidadeTest.java
@@ -10,7 +10,14 @@ public class ProfessorEntidadeTest {
 
     @Test
     public void testEquals() {
-        
+        Professor professor = CriaObjetoTeste.criaProfessor();
+        Entidade entidade = CriaObjetoTeste.criaEntidade();
+
+        professorEntidade.setProfessor(professor);
+        professorEntidade.setEntidade(entidade);
+
+        assertEquals(professorEntidade.getProfessor(), professor);
+        assertEquals(professorEntidade.getEntidade(), entidade);
     }
     
     @Test
